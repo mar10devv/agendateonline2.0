b9e5aececd493d91b02c595b568d887fe8815ea7
mar10devv <mar10devcontacto@gmail.com>
2025-09-08 22:21:12 -0300

    ya no se

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import netlifyFunctions from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
+// Detecta si estás en Netlify
 const isNetlify = process.env.NETLIFY === "true";
 
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({ applyBaseStyles: true }),
+    tailwind({
+      applyBaseStyles: true,
+    }),
   ],
+  // 👇 Si estás en local = static. En Netlify = server con adapter
   output: isNetlify ? "server" : "static",
-  adapter: isNetlify ? netlifyFunctions({}) : undefined,
+  adapter: isNetlify ? netlify() : undefined,
 });
